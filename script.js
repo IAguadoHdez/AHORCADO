d60d2a2906a1cacb9e7486e1505fd94560761386
Iker Aguado <iaguado20938@iesjoanramis.org>
2025-11-06 16:30:46 +0100

    Agregado cronometro

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const letras = document.querySelectorAll(".letra");
   const intentosSpan = document.querySelector(".intentos span");
   const erroresSpan = document.querySelector(".erroresCometidos span"); 
-
+  const tiempoSpan = document.getElementById("tiempo");
+  const PopupUsuario = document.querySelector(".contenedor-sesion");
+  const btnUsuario = document.querySelector(".sesion-usuario button");
   const palabra = "PACO";
 
   let progreso = Array(palabra.length).fill("_"); // crea un guion por cada letra
@@ -29,13 +31,53 @@ document.addEventListener("DOMContentLoaded", () => {
     erroresSpan.innerText = errores;
   }
 
+  // Contador 
+  let segundos = 0;
+  let minutos = 0;
+  let intervalo = null;
+  let iniciado = false;
+
+  function actualizarTiempo() {
+    let seg = segundos < 10 ? "0" + segundos : segundos; // Esto hace que si los segundos son menores a 10 se agregue el 0 delante del numero ejemplo si es 7 saldria "07"
+    let min = minutos < 10 ? "0" + minutos : minutos; // Esto hace lo mismo pero con los minutos
+    tiempoSpan.innerText = `${min}:${seg}`; 
+  }
+
+  function iniciarCronometro() {
+    if(iniciado) return; // Si ya esta iniciado que no haga nada
+    iniciado = true;
+    //Crear el intervalo que se ejecuta cada segundo que pasa
+    intervalo = setInterval(() => {
+      segundos++;
+
+      // Esto hace que cuando los segundos marcan 60 pues vuelve a 0 para poder seguir con el minuto que se suma
+      if(segundos == 60) {  
+        segundos = 0;
+        minutos++;
+      }
+      actualizarTiempo();
+    }, 1000);
+  }
+
+  function detenerCrono() {
+    clearInterval(intervalo); // Elimina intervalo
+  }
+
+
+  
   mostrarProgreso();
   actualizarContadores();
-
+  actualizarTiempo();
+  
   // Eventos 
+
+  btnUsuario.addEventListener("click", () => {
+      PopupUsuario.style.display = "none";
+  })
 
   letras.forEach((letra) => {
     letra.addEventListener("click", () => {
+      iniciarCronometro();
       const letraSeleccionada = letra.innerText;
 
       // Si la letra ya fue seleccionada o bloqueada, no hacer nada
@@ -52,17 +94,18 @@ document.addEventListener("DOMContentLoaded", () => {
         mostrarProgreso();
 
         // Verificar si he ganado
-        if (!progreso.includes("_")) console.log("Has ganado!");
+        if (!progreso.includes("_")) {
+          detenerCrono();
+        }
       } else {
-        letra.classList.add("bloqueado"); // letra incorrecta
+        letra.classList.add("bloqueado");
 
-        // Solo sumamos error si aún no llegó al máximo
         if (errores < intentosMaximos) errores++;
-
         actualizarContadores();
 
-        // Verificar si he perdido
-        if (errores >= intentosMaximos) console.log(`Has perdido, la palabra era: ${palabra}`);
+        if (errores >= intentosMaximos) {
+          detenerCrono();
+        }
       }
     });
   });
