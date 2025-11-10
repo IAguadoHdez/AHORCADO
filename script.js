9919c258ab6db588edef949e2c3752883a73d70a
Iker Aguado <axel.goku2391@gmail.com>
2025-11-10 10:23:57 +0100

    Agregado validación de usuario y usuarios guardados al localStorage

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const tiempoSpan = document.getElementById("tiempo");
   const PopupUsuario = document.querySelector(".contenedor-sesion");
   const btnUsuario = document.querySelector(".sesion-usuario button");
-  const palabra = "PACO";
+  const nombreUsuarioInput = document.querySelector(".sesion-usuario input");
+  const errorUsuario = document.querySelector(".error-usuario");
 
+  // Datos del juego
+  const palabra = "PAPER";
   let progreso = Array(palabra.length).fill("_"); // crea un guion por cada letra
   let errores = 0; 
   const intentosMaximos = 6; 
 
+  // Datos usuario 
+  let usuario = null;
+  let usuarios = JSON.parse(localStorage.getItem("usuarios"))|| []; // Esto recupera el almacenamiento del local storage y si no hay nada dentro crea el array
+
   // Mostrar la palabra en pantalla
   function mostrarProgreso() {
-    let texto = "";
-    progreso.forEach((letra) => {
+    let texto = ""; // Inicializo el texto
+    progreso.forEach((letra) => { 
       texto += letra + " "; 
     });
     palabraOculta.innerText = texto.trim();
@@ -25,13 +32,13 @@ document.addEventListener("DOMContentLoaded", () => {
   function actualizarContadores() {
     // evitar que los errores o intentos restantes sean negativos
     if (errores < 0) errores = 0;
-    const intentosRestantes = intentosMaximos - errores >= 0 ? intentosMaximos - errores : 0;
+    const intentosRestantes = intentosMaximos - errores >= 0 ? intentosMaximos - errores : 0; // calcula los intentos restantes, pero si el resultado llegase a ser negativo usa 0 en vez de dejarlo en negativo
 
     intentosSpan.innerText = intentosRestantes; 
     erroresSpan.innerText = errores;
   }
 
-  // Contador 
+  // Datos contador 
   let segundos = 0;
   let minutos = 0;
   let intervalo = null;
@@ -63,7 +70,28 @@ document.addEventListener("DOMContentLoaded", () => {
     clearInterval(intervalo); // Elimina intervalo
   }
 
+  function UsuarioValido(nombre){
+    const valido = nombre && nombre.trim().length >= 3;
+    if(!valido) {
+      errorUsuario.innerText = "El usuario debe tener al menos 3 caracteres";
+      errorUsuario.style.display = "block";
+    } 
 
+    return valido;
+  }
+
+  function guardarUsuario() {
+    const tiempoFinal = `${minutos < 10 ? "0" + minutos : minutos}:${segundos < 10 ? "0" + segundos : segundos}`; // formatear tiempo
+    const datoUsuario = {
+      nombre: usuario, // nombre del jugador
+      palabra: palabra, // palabra jugada
+      tiempo: tiempoFinal, // tiempo de la partida
+      errores: errores // errores cometidos
+    };
+    usuarios.push(datoUsuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    console.log(usuarios); 
+  }
   
   mostrarProgreso();
   actualizarContadores();
@@ -72,8 +100,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // Eventos 
 
   btnUsuario.addEventListener("click", () => {
-      PopupUsuario.style.display = "none";
-  })
+    const nombreIngresado = nombreUsuarioInput.value.trim(); // obtener valor input
+    // validar el nombre ingresado y mostrar error en el modal si corresponde
+    if (UsuarioValido(nombreIngresado)) {
+      usuario = nombreIngresado; // asignar usuario actual
+      PopupUsuario.style.display = "none"; 
+    }
+  });
+
 
   letras.forEach((letra) => {
     letra.addEventListener("click", () => {
@@ -94,17 +128,19 @@ document.addEventListener("DOMContentLoaded", () => {
         mostrarProgreso();
 
         // Verificar si he ganado
-        if (!progreso.includes("_")) {
+        if (!progreso.includes("_")) { // Si no hay más guiones que añadir pues has adivinado la palabra
           detenerCrono();
+          guardarUsuario();
         }
       } else {
-        letra.classList.add("bloqueado");
+        letra.classList.add("bloqueado"); // Si hay errores pues sigue dando error en la letra
 
-        if (errores < intentosMaximos) errores++;
-        actualizarContadores();
+        if (errores < intentosMaximos) errores++; // Aumenta el contador de errores
+        actualizarContadores(); // Actualiza  para mostrar los valores por pantalla
 
         if (errores >= intentosMaximos) {
           detenerCrono();
+          guardarUsuario();
         }
       }
     });
